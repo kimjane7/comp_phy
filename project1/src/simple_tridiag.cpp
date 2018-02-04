9ffdc937f7e5ffb42ba191a2b678d29e98e8f0d8
Jane Kim <kimjane7@msu.edu>
2018-02-03 22:02:47 -0500

    simple program works now!

--- project1/src/simple_tridiag.cpp
+++ project1/src/simple_tridiag.cpp
@@ -1,9 +1,8 @@
 //  Author: Jane Kim
 
 //	This program approximates the solution to the 1D Poisson equation with 
-//  Dirichlet boundary conditions. This algorithm assumes the matrix is identically
-//  -2 on the main diagonal and identically 1 on the off-diagonals.
-
+//  Dirichlet boundary conditions. The algorithm assumes the matrix is tridiagonal, 
+//  with 2's along the main diagonal and -1's along the two off-diagonals.
 
 #include <iostream>
 #include <cmath>
@@ -13,17 +12,23 @@
 #include <fstream>
 #include <iomanip>
 
-
 using namespace std;
+
+// test function
+double f(double x) { return 100.0*exp(-10.0*x); }
+
+// closed-form solution of -u''(x)=f(x)
+double solution(double x) { return 1.0-(1.0-exp(-10.0))*x-exp(-10.0*x); }
 
 int main(int argc, char *argv[]){
 
 	int max = atoi(argv[1]);	 // highest power of 10
 	string filename = argv[2];   // base file name
-	long int n;					 // dimension of matrix
+
+	int n;					     // dimension of matrix
 	double h, hh;		    	 // spacing, spacing squared
-	double f;					 // place holder for function value
 	double err;					 // place holder for relative error
+	double factor; 				 // place holder for precalculated factor
 
 	ofstream ofile;
 
@@ -32,56 +37,55 @@ int main(int argc, char *argv[]){
 
 		string outfile = filename + to_string(i) + ".dat";
 
-		n = (int) pow(10.0,i); // is int large enough for n?
+		n = (int) pow(10.0,i);			  
 		h = 1.0/n;
 		hh = h*h;
 
-		double *x = new double[n]; // function inputs
-		double *b = new double[n]; // Av=b
-		double *d = new double[n]; 
-		double *v = new double[n]; 
-		double *v_exact = new double[n]; 
+		double *x = new double[n];        // function inputs
+		double *v_approx = new double[n]; // Av=b
+		double *v_exact = new double[n];  // closed-form solution
 
-		// set-up
+		double *b = new double[n];        // Av=b
+		double *d = new double[n];        // diagonal
+
+		// set up
 		for(int j = 0; j < n; j++){
 
-			// fill out vectors
 			x[j] = (j+0.5)*h;
-			f = 100.0*exp(-10.0*x[j]);
-			b[j] = hh*f;
-			v_exact[j] = 1.0-(1.0-exp(-10.0))*x[j]-exp(-10*x[j]);
-
+			b[j] = hh*f(x[j]);
+			v_exact[j] = solution(x[j]);
+			d[j]=(j+2.0)/(j+1.0);
 		}
 
-		d[0] = 2.0;
-		for(int j = 1; j < n-1; j++){
-			d[j] = (j+1.0)/j;
+		// forward substitution
+		for(int j = 1; j < n; j++){
+
+			b[j] = b[j]+b[j-1]/d[j-1];
 		}
-
-
-		// forward subsitution
-		for(int j = 2; j < n; j++) b[j] = b[j]+b[j-1]/d[i-1] ;
 
 		// backward substitution
-		v[n-1] = b[n-1]/d[n-1];
-		for(int j = n-2; j > 0; j--) v[j] = (b[j]+v[j+1])/d[i] ;
+		v_approx[n-1] = b[n-1]/d[n-1];
+		for(int j = n-2; j >= 0; j--){
+
+			v_approx[j] = (b[j]+v_approx[j+1])/d[j];
+		}
 
 		// print 
 		ofile.open(outfile);
-		ofile << "# x, approximate solution, exact solution, log(error)" << endl;
 		for(int j = 0; j < n; j++){
-			err = fabs((v[j]-v_exact[j])/v_exact[j]);
+			err = fabs((v_approx[j]-v_exact[j])/v_exact[j]);
 			ofile << setw(15) << setprecision(8) << x[j];
-			ofile << setw(15) << setprecision(8) << v[j];
+			ofile << setw(15) << setprecision(8) << v_approx[j];
 			ofile << setw(15) << setprecision(8) << v_exact[j];
 			ofile << setw(15) << setprecision(8) << log10(err) << endl;
 		}
-		ofile.close();	
-		
 
+		ofile.close();
+
+		delete [] v_approx; delete [] v_exact;
+		delete [] x; delete [] b; delete [] d;
 
 	}
-	
 
 	return 0;
 }
