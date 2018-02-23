1026ed7331a100ce78f1afff344c12630d76c8bb
Jane Kim <kimjane7@msu.edu>
2018-02-23 18:59:16 -0500

    running now, but running wrong

--- project2/src/classic_jacobi.cpp
+++ project2/src/classic_jacobi.cpp
@@ -41,31 +41,61 @@ double norm_sq(mat& A, int n){
 	}
 
 	// add squares of off-diagonal elements
-	norm_sq += offdiag_sq(&A,n);
+	norm_sq += offdiag_sq(A,n);
 
 	return norm_sq;
 }
 
-// stores the index of the max off-diagonal element of each column
-// only need to change p(k) and p(l) after each rotation
-void get_pivots(mat &A, ivec &p, int k, int l, int n){
+void get_pivots(mat& A, ivec& p, int n){
 
-	double kmax, lmax, Aik, Ail;
+	int jmax; 		      // placeholder for index of largest element
+	double imax = 0.0;    // value of max element in row i
+	double Aij;
 
-	for(int i = 0; i < n; i++){
-
-		Aik = fabs(A(i,k));
-		Ail = fabs(A(i,l));
-
-		if( Aik > kmax ) { kmax = Aik; }
-		if( Ail > lmax ) { lmax = Ail; }
+	// p(i) = index of largest off-diag elem in row i
+	// A is symmetric, so only consider elements above diagonal
+	for(int i = 0; i < n-1; i++){
+		for(int j = i+1; j < n; j++){
+			Aij = fabs(A(i,j));
+			if(Aij > imax){
+				imax = Aij; 
+				jmax = j;
+			}
+		}
+		p(i) = jmax;
+		imax = 0.0;
 	}
-
-	p(k) = kmax;
-	p(l) = lmax;
 }
 
-void rotate(mat &A, mat &V, ivec &p, int k, int l, int n){
+void update_pivots(mat& A, ivec& p, int k, int l, int n){
+
+	double kmax = 0.0, lmax = 0.0;
+	double Akj, Alj;
+
+	for(int i = 0; i < n; i++){
+		if(i = k){
+			for(int j = l+1; j < n; j++){
+				Akj = fabs(A(k,j));
+				if(Akj > kmax) kmax = j;
+			}
+			p(k) = kmax;
+		}
+		if( (i=l) && (l<n-1))
+	}
+
+
+
+	// l may equal n-1
+	if(l < n-1){
+		for(int j = l+1; j < n; j++){
+			Alj = fabs(A(l,j));
+			if(Alj > lmax) lmax = j;
+		}
+		p(l) = lmax;	
+	}
+}
+
+void rotate(mat& A, mat& V, ivec& p, int k, int l, int n){
 
 	// zero out A(k,l)=A(l,k)
 	if( A(k,l) != 0.0 ){
@@ -87,7 +117,8 @@ void rotate(mat &A, mat &V, ivec &p, int k, int l, int n){
 		s = t*c;
 
 		// perform rotation
-		A(k,l) = A(l,k) = 0;
+		A(k,l) = 0.0;
+		A(l,k) = 0.0;
 		A(k,k) = cc*Akk+ss*All-2.0*cs*Akl;
 		A(l,l) = ss*Akk+cc*All+2.0*cs*Akl;
 		for(int i = 0; i < n; i++){
@@ -97,8 +128,8 @@ void rotate(mat &A, mat &V, ivec &p, int k, int l, int n){
 				Ail = A(i,l);
 				A(i,k) = c*Aik-s*Ail;
 				A(i,l) = c*Ail+s*Aik;
-				A(k,i) = Aik;
-				A(l,i) = Ail;
+				A(k,i) = A(i,k);
+				A(l,i) = A(i,l);
 			}
 
 			// rotate eigenvectors 
@@ -109,50 +140,97 @@ void rotate(mat &A, mat &V, ivec &p, int k, int l, int n){
 		}
 	}
 
-	else( cout << "These elements are already zero!" << endl; )
+	else{ cout << "These elements are already zero!" << endl; } 
 }
 
+void print_matrix(mat& A, int n){
+
+	for(int i = 0; i < n; i++){
+		cout << "[";
+		for(int j = 0; j < n; j++){
+			cout << setw(10) << setprecision(3) << A(i,j);
+		}
+		cout << "]" << endl;
+	}
+	cout << "\n" << endl;
+}
 
 int main(int argc, char *argv[]){
 
-	int N;
+	int n, k, l, iterations = 0;
+	double max_offdiag, Aij; 
 
 	if(argc < 1){ cout << "Input dimension of matrix." << endl; }
-	else{ N = atoi(argv[1]); }
+	else{ n = atoi(argv[1]); }
 
-	double h = 1.0/N, hh = h*h;
-	double time, epsilon;
+	double h = 1.0/n, hh= h*h;
+	double d = 2.0/hh, a = -1.0/hh;
+	double time, lambda, epsilon = 10E-10;
 
-	// set up matrix
-	mat A = zeros<mat>(N,N);
-	for(int i = 0; i < N-1; i++){
-		A(i,i) = 2.0;
-		A(i+1,i) = -1.0;
-		A(i,i+1) = -1.0;
+	// set-up matrix to diagonalize
+	mat A = zeros<mat>(n,n);
+	for(int i = 0; i < n-1; i++){
+		A(i,i) = d;
+		A(i+1,i) = a;
+		A(i,i+1) = a;
 	}
-	A(N-1,N-1) = 2.0;
+	A(n-1,n-1) = d;
+
+	print_matrix(A,n);
+
+	// set-up matrix of eigenvectors
+	mat V = eye<mat>(n,n);
+
+	// set-up list of indices of the max off-diag elem of each column
+	ivec p(n-1);
+	get_pivots(A, p, n);
 
 	// start timer
 	clock_t initial, final;
 	initial = clock();
 
-	// solve for eigenvalues
-	double tau, t, s, c;
+	// solve for eigenvalues and eigenvectors
+	while(offdiag_sq(A,n) > epsilon){
 
+		// find indices (k,l) of max pivot
+		max_offdiag = 0.0;
+		for(int i = 0; i < n-1; i++){
+			Aij = fabs(A(i,p(i)));
+			if(Aij > max_offdiag){
+				max_offdiag = Aij;
+				k = i;
+				l = p(i);
+			}
+		}
+
+		cout << "(k,l) = " << k << "\t" << l << endl;
+
+		// apply rotation
+		rotate(A, V, p, k, l, n);
+
+		print_matrix(A,n);
+
+		// update list of pivots
+		update_pivots(A, p, k, l, n);
+
+		iterations += 1;
+
+	}
 
 
 	// end timer
 	final = clock();
 	time = (final-initial)/((double) CLOCKS_PER_SEC);
+	cout << "Diagonalized in " << iterations << " iterations" << endl;
 	cout << "Computation Time = " << time << " s" << endl;
 
 	// print to check if eigenvalues are correct
 	cout << "Calculated:\tExact:" << endl;
-	for(int i = 0; i < N; i++){
+	for(int i = 0; i < n; i++){
 
-		lambda = d+2.0*a*cos((i+1)*pi/(N+1));
+		lambda = d+2.0*a*cos((i+1)*pi/(n+1));
 
-		cout << eigval(i)/hh << "\t\t" << lambda  << endl;
+		cout << A(i,i) << "\t\t" << lambda  << endl;
 	}
 
 	return 0;
