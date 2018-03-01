d33baccaf7c4619b11c2ced211eb143875880642
Jane Kim <kimjane7@msu.edu>
2018-03-01 14:11:01 -0500

    classic jacobi algorithm working right.

--- project2/src/classic_jacobi.cpp
+++ project2/src/classic_jacobi.cpp
@@ -46,56 +46,23 @@ double norm_sq(mat& A, int n){
 	return norm_sq;
 }
 
-void get_pivots(mat& A, ivec& p, int n){
+void get_pivot(mat& A, int n, int& k, int& l){
 
-	int jmax; 		      // placeholder for index of largest element
-	double imax = 0.0;    // value of max element in row i
-	double Aij;
+	double Aij,max_offdiag = 0.0;
 
-	// p(i) = index of largest off-diag elem in row i
-	// A is symmetric, so only consider elements above diagonal
 	for(int i = 0; i < n-1; i++){
 		for(int j = i+1; j < n; j++){
 			Aij = fabs(A(i,j));
-			if(Aij > imax){
-				imax = Aij; 
-				jmax = j;
+			if(Aij > max_offdiag){
+				max_offdiag = Aij;
+				k = i;
+				l = j;
 			}
 		}
-		p(i) = jmax;
-		imax = 0.0;
 	}
 }
 
-void update_pivots(mat& A, ivec& p, int k, int l, int n){
-
-	double kmax = 0.0, lmax = 0.0;
-	double Akj, Alj;
-
-	for(int i = 0; i < n; i++){
-		if(i = k){
-			for(int j = l+1; j < n; j++){
-				Akj = fabs(A(k,j));
-				if(Akj > kmax) kmax = j;
-			}
-			p(k) = kmax;
-		}
-		if( (i=l) && (l<n-1))
-	}
-
-
-
-	// l may equal n-1
-	if(l < n-1){
-		for(int j = l+1; j < n; j++){
-			Alj = fabs(A(l,j));
-			if(Alj > lmax) lmax = j;
-		}
-		p(l) = lmax;	
-	}
-}
-
-void rotate(mat& A, mat& V, ivec& p, int k, int l, int n){
+void rotate(mat& A, mat& V, int k, int l, int n){
 
 	// zero out A(k,l)=A(l,k)
 	if( A(k,l) != 0.0 ){
@@ -107,8 +74,8 @@ void rotate(mat& A, mat& V, ivec& p, int k, int l, int n){
 
 		// calculate angle of rotation
 		tau = 0.5*(All-Akk)/Akl;
-		if(tau >= 0.0){ t = tau+sqrt(1.0+tau*tau); }
-		else{ t = tau-sqrt(1.0+tau*tau); }
+		if(tau >= 0.0){ t = -tau+sqrt(1.0+tau*tau); }
+		else{ t = -tau-sqrt(1.0+tau*tau); }
 
 		cc = 1.0/(1.0+t*t);
 		ss = 1.0-cc;
@@ -152,7 +119,35 @@ void print_matrix(mat& A, int n){
 		}
 		cout << "]" << endl;
 	}
-	cout << "\n" << endl;
+	cout << endl;
+}
+
+void print_eigenvalues(mat& A, int n, double a, double d){
+
+	vec eigvals(n);
+	double lambda;
+
+	for(int i = 0; i < n; i++){
+		eigvals(i) = A(i,i);
+	}
+
+	sort(eigvals.begin(),eigvals.end());
+
+	// check if eigenvalues are correct
+	cout << left << "* EIGENVALUES *\n";
+	cout << showpoint;
+	cout << setw(15) << "Calculated:";
+	cout << setw(15) << "Exact:" << endl;
+
+	for(int i = 0; i < n; i++){
+
+		lambda = d+2.0*a*cos((i+1)*pi/(n+1));
+
+		cout << setprecision(10) << setw(15) << eigvals(i); 
+		cout << setprecision(10) << setw(15) << lambda << endl;
+	}
+
+	cout << endl;
 }
 
 int main(int argc, char *argv[]){
@@ -165,7 +160,7 @@ int main(int argc, char *argv[]){
 
 	double h = 1.0/n, hh= h*h;
 	double d = 2.0/hh, a = -1.0/hh;
-	double time, lambda, epsilon = 10E-10;
+	double time, epsilon = 1E-5;
 
 	// set-up matrix to diagonalize
 	mat A = zeros<mat>(n,n);
@@ -176,14 +171,9 @@ int main(int argc, char *argv[]){
 	}
 	A(n-1,n-1) = d;
 
-	print_matrix(A,n);
-
 	// set-up matrix of eigenvectors
 	mat V = eye<mat>(n,n);
-
-	// set-up list of indices of the max off-diag elem of each column
-	ivec p(n-1);
-	get_pivots(A, p, n);
+	
 
 	// start timer
 	clock_t initial, final;
@@ -192,46 +182,27 @@ int main(int argc, char *argv[]){
 	// solve for eigenvalues and eigenvectors
 	while(offdiag_sq(A,n) > epsilon){
 
-		// find indices (k,l) of max pivot
-		max_offdiag = 0.0;
-		for(int i = 0; i < n-1; i++){
-			Aij = fabs(A(i,p(i)));
-			if(Aij > max_offdiag){
-				max_offdiag = Aij;
-				k = i;
-				l = p(i);
-			}
-		}
+		get_pivot(A, n, k, l);
 
-		cout << "(k,l) = " << k << "\t" << l << endl;
+		rotate(A, V, k, l, n);
 
-		// apply rotation
-		rotate(A, V, p, k, l, n);
-
-		print_matrix(A,n);
-
-		// update list of pivots
-		update_pivots(A, p, k, l, n);
+		// print_matrix(A,n);
 
 		iterations += 1;
-
 	}
-
 
 	// end timer
 	final = clock();
 	time = (final-initial)/((double) CLOCKS_PER_SEC);
-	cout << "Diagonalized in " << iterations << " iterations" << endl;
-	cout << "Computation Time = " << time << " s" << endl;
+	cout << "\nComputation Time = " << time << " s\n";
 
-	// print to check if eigenvalues are correct
-	cout << "Calculated:\tExact:" << endl;
-	for(int i = 0; i < n; i++){
+	cout << "Diagonalized in " << iterations << " iterations\n" << endl;
 
-		lambda = d+2.0*a*cos((i+1)*pi/(n+1));
+	print_eigenvalues(A,n,a,d);
 
-		cout << A(i,i) << "\t\t" << lambda  << endl;
-	}
+	// cout << "* EIGENVECTORS *" << endl;
+	// print_matrix(V,n);
+
 
 	return 0;
 }
